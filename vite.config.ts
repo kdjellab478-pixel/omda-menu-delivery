a57b97b6fd3509314476dcd5359538b799dd14a6
kdjellab478-pixel <kdjellab478@gmail.com>
2025-11-10 19:43:17 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,21 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
-import { componentTagger } from "lovable-tagger";
+import componentTagger from "lovable-tagger";
 
 // https://vitejs.dev/config/
-export default defineConfig(({ mode }) => ({
+export default defineConfig({
+  // 👇 هذا السطر يحل مشكلة المسارات (404) على GitHub Pages
+  base: './', 
+  
   server: {
-    host: "::",
+    host: "*.*",
     port: 8080,
   },
-  plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
+  plugins: [react(), mode => (mode === 'development' && componentTagger() || false) as any],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
-}));
+});
